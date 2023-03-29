0fa982aae30be5314e5838be6c733287d52c8f15
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-03-30 00:41:42 +0100

    Create 'publicationsInfoArray' file

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -1,25 +1,8 @@
 import uniqid from "uniqid";
-import { RiArticleLine } from "react-icons/ri";
-import { VscFolderLibrary } from "react-icons/vsc";
-import { GiBlackBook } from "react-icons/gi";
 import { Nunito } from "next/font/google";
+import publicationsInfoArray from "../publicationsInfoArray";
 
 const nunito = Nunito({ subsets: ["latin"] });
-const publicationsInfoArray = [
-  {
-    icon: <RiArticleLine />,
-    name: "Articles",
-  },
-  {
-    icon: <VscFolderLibrary />,
-    name: "Libraries",
-  },
-  {
-    icon: <GiBlackBook />,
-    name: "Books",
-  },
-];
-
 const publicationSpans = publicationsInfoArray.map((i) => (
   <span key={uniqid()}>
     {i.icon}
---
+++ src/publicationsInfoArray.js
@@ -0,0 +1,20 @@
+import { RiArticleLine } from "react-icons/ri";
+import { VscFolderLibrary } from "react-icons/vsc";
+import { GiBlackBook } from "react-icons/gi";
+
+const publicationsInfoArray = [
+  {
+    icon: <RiArticleLine />,
+    name: "Articles",
+  },
+  {
+    icon: <VscFolderLibrary />,
+    name: "Libraries",
+  },
+  {
+    icon: <GiBlackBook />,
+    name: "Books",
+  },
+];
+
+export default publicationsInfoArray;
