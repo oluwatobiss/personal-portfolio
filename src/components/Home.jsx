b63f4b9813bd36384b6d90ef4e5e9989d1a9c240
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-03-29 13:48:57 +0100

    Create the projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Image from "next/image";
+import transformProjPic from "@/public/transform-oluwatobi-sofela.png";
+import findProjPic from "@/public/find-oluwatobi-sofela.png";
+import youtubeProjPic from "@/public/youtube-clone-oluwatobi-sofela.png";
 import styles from "@/src/styles/Home.module.css";
 import { Nunito } from "next/font/google";
 
@@ -28,6 +32,61 @@ export default function Home() {
           <p className={nunito.className}>
             A JavaScript Front End Web Developer
           </p>
+        </div>
+      </section>
+      <section className={styles.projectSection}>
+        <h2>Selected Projects</h2>
+        <div>
+          <span className={styles.infoSpan}>
+            <h3>Transform.</h3>
+            <small>ReactJS, React Router</small>
+            <p className={nunito.className}>
+              Transform is a shopping cart website where users browse for
+              glamorous items to spark up their style. You can use it to
+              transform your clothing, jewelry, and electronic collections.
+            </p>
+          </span>
+          <span className={styles.imageSpan}>
+            <Image
+              src={transformProjPic}
+              alt="Transform shopping cart project"
+              placeholder="blur"
+            />
+          </span>
+        </div>
+        <div>
+          <span className={styles.infoSpan}>
+            <h3>Find</h3>
+            <small>Firebase, Jest, ReactJS, Testing Library</small>
+            <p className={nunito.className}>
+              Where is Waldo is a search-and-find game where a player searches
+              an image for some items (e.g., Waldo, Cat, Apple, etc.). You
+              finish the game by finding all the specified elements.
+            </p>
+          </span>
+          <span className={styles.imageSpan}>
+            <Image src={findProjPic} alt="Find project" placeholder="blur" />
+          </span>
+        </div>
+        <div>
+          <span className={styles.infoSpan}>
+            <h3>YouTube Clone</h3>
+            <small>Firebase, Jest, ReactJS, Testing Library</small>
+            <p className={nunito.className}>
+              YouTube Clone is a replication of the YouTube website. This
+              replica implements most of the live website's functionality, like
+              authentication, search, history, and shorts. So, you can use it to
+              watch your favorite videos just as you would with the actual
+              website.
+            </p>
+          </span>
+          <span className={styles.imageSpan}>
+            <Image
+              src={youtubeProjPic}
+              alt="Youtube project"
+              placeholder="blur"
+            />
+          </span>
         </div>
       </section>
     </>
