e4d6d29be1c11d627130879a480627216f7f387e
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-04-10 19:37:48 +0100

    Create 'Milestones' CSS module

--- src/components/Milestones.jsx
+++ src/components/Milestones.jsx
@@ -1,5 +1,7 @@
 import Layout from "./Layout";
 import Head from "next/head";
+import styles from "@/src/styles/Milestones.module.css";
+import { GiDiamondTrophy } from "react-icons/gi";
 
 export default function Milestones() {
   return (
@@ -7,7 +9,25 @@ export default function Milestones() {
       <Head>
         <title>Milestones - Oluwatobi Sofela</title>
       </Head>
-      <h1>Milestones</h1>
+      <h1 className={styles.pageHeading}>Milestones</h1>
+      <div className={styles.milestonesContainer}>
+        <section className={styles.milestoneCard}>
+          <span className={styles.milestoneIcon}>
+            <GiDiamondTrophy />
+          </span>
+          <span className={styles.milestoneInfo}>
+            <span className={styles.date}>2022-12-02</span>
+            <a
+              href="https://www.freecodecamp.org/news/freecodecamp-2022-top-contributors/#:~:text=Keyur%20Paralkar-,Oluwatobi%20Sofela,-Eamonn%20Cottrell"
+              target="_blank"
+              rel="noopener"
+              className={styles.title}
+            >
+              2022 Top Contributors Award - freeCodeCamp
+            </a>
+          </span>
+        </section>
+      </div>
     </Layout>
   );
 }
