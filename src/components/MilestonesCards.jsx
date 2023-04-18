65856ec7c1ff9bbd18248bc701be7f3a0789ecc6
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-04-18 20:22:58 +0100

    Make 'milestones' page mobile responsive

--- src/components/MilestonesCards.jsx
+++ src/components/MilestonesCards.jsx
@@ -14,13 +14,25 @@ const milestonesCards = allMilestonesInDescOrder.map((i) => {
       className={`${styles.milestoneCard} card-container milestone-card`}
       key={uniqid()}
     >
-      <span className={styles.milestoneIcon}>{i.icon}</span>
-      <span className={styles.milestoneInfo}>
+      <div className={styles.milestoneIcon}>{i.icon}</div>
+      <div className={styles.milestoneInfo}>
         <span>{i.date}</span>
         <a href={i.link} target="_blank" rel="noopener">
-          {i.title}
+          {i.title}{" "}
+          <svg
+            width="13.5"
+            height="13.5"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            className={styles.externalLink}
+          >
+            <path
+              fill="currentColor"
+              d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"
+            ></path>
+          </svg>
         </a>
-      </span>
+      </div>
     </section>
   );
 });
