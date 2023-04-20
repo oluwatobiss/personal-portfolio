018196d7cb3a1d941416fbcff32052d11b9fbc8a
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-04-20 20:21:53 +0100

    Show loading progress bar on route change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import Layout from "./Layout";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <NextNProgress color="#e53a35" />
       <Component {...pageProps} />
     </Layout>
   );
